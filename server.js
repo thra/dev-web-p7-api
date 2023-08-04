36b594d626f8101fed4870138187840749485b5a
tr-pyramidanatomy <tr.prof06@gmail.com>
2023-08-04 19:26:51 +0200

    update server https

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const normalizePort = val => {
   }
   return false;
 };
-const port = normalizePort(process.env.PORT || '4000');
+const port = normalizePort(process.env.PORT || '4000');
 app.set('port', port);
 
 const errorHandler = error => {
@@ -35,7 +35,22 @@ const errorHandler = error => {
   }
 };
 
-const server = http.createServer(app);
+const isHTTP = process.env.NODE_ENV !== 'prod'
+
+// const server = http.createServer(app);
+
+// start http or https server
+const server = isHTTP ?
+  // http config
+  http.createServer(app) :
+  // otherwise https config
+  https.createServer({
+    // options https://nodejs.org/api/tls.html#tls_tls_createsecurecontext_options
+    // https://itnext.io/node-express-letsencrypt-generate-a-free-ssl-certificate-and-run-an-https-server-in-5-minutes-a730fbe528ca
+    key: fs.readFileSync(path.resolve("/home/ubuntu/live/matouba-portfolio.fr/privkey.pem")),
+    cert: fs.readFileSync(path.resolve("/home/ubuntu/live/matouba-portfolio.fr/cert.pem")),
+    ca: fs.readFileSync(path.resolve("/home/ubuntu/live/matouba-portfolio.fr/chain.pem"))
+  }, app)
 
 server.on('error', errorHandler);
 server.on('listening', () => {
