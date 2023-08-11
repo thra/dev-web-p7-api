d843492db209928485df34e07b2d3d12c377f3d2
tr-pyramidanatomy <tr.prof06@gmail.com>
2023-08-11 19:38:53 +0200

    update mutler and sharp middleware

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,21 +1,25 @@
 const multer = require('multer');
 
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
+// const MIME_TYPES = {
+//   'image/jpg': 'jpg',
+//   'image/jpeg': 'jpg',
+//   'image/png': 'png'
+// };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images');
-  },
-  filename: (req, file, callback) => {
-    console.log("file:", file)
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, 'images');
+//   },
+//   filename: (req, file, callback) => {
+//     console.log("file:", file)
+//     const name = file.originalname.split(' ').join('_');
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + '.' + extension);
+//   }
+// });
 
-module.exports = multer({ storage: storage }).single('image');
+// module.exports = multer({ storage: storage }).single('image');
+
+const storage = multer.memoryStorage()
+
+module.exports = multer({ storage }).single('image')
--- middleware/sharp-config.js
+++ middleware/sharp-config.js
@@ -1,25 +1,50 @@
-const sharp = require('sharp');
-const fs = require('fs');
+// const sharp = require('sharp');
+// const fs = require('fs');
 
-const convertToWebp = async (req, res, next) => {
-  if (!req.file) return next();
+// const convertToWebp = async (req, res, next) => {
+//   if (!req.file) return next();
 
-  console.log("sharp - req.file:", req.file)
+//   console.log("sharp - req.file:", req.file)
 
-  const inputFile = req.file.path;
-  const outputFile = inputFile.replace(/\.(jpg|jpeg|png)$/, '.webp');
+//   const inputFile = req.file.path;
+//   const outputFile = inputFile.replace(/\.(jpg|jpeg|png)$/, '.webp');
 
-  try {
-    await sharp(inputFile).toFormat('webp').toFile(outputFile);
-    fs.unlinkSync(inputFile);
+//   try {
+//     await sharp(inputFile).toFormat('webp').toFile(outputFile);
+//     fs.unlinkSync(inputFile);
 
-    req.file.path = outputFile;
-    req.file.mimetype = 'image/webp';
-    req.file.filename = req.file.filename.replace(/\.(jpg|jpeg|png)$/, '.webp');
-  } catch (err) {
-    console.error("Error while converting image:", err);
+//     req.file.path = outputFile;
+//     req.file.mimetype = 'image/webp';
+//     req.file.filename = req.file.filename.replace(/\.(jpg|jpeg|png)$/, '.webp');
+//   } catch (err) {
+//     console.error("Error while converting image:", err);
+//   }
+//   next();
+// };
+
+// module.exports = convertToWebp;
+
+const sharp = require("sharp")
+const path = require('path')
+
+module.exports = (req, res, next) => {
+
+  console.log("req.file:", req.file)
+  if (!req.file) {
+    next()
+    return
   }
-  next();
-};
 
-module.exports = convertToWebp;
+  const { buffer, fieldname } = req.file;
+  const filename = `${fieldname}-${Date.now()}-${Math.round(Math.random() * 1E9)}.webp`;
+  sharp(buffer)
+    .webp()
+    .toFile(path.resolve(__dirname, `../images/${filename}`), (err, info) => {
+      console.log(err || info)
+      if (err) {
+        return res.status(400).json({ error: err })
+      }
+      req.file.filename = filename
+      next()
+    })
+}
